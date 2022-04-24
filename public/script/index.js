4fd77f30bb0c32a7be79f96019950fc88f4c2c29
Mindaugas <mindaugas.antanas.grigaitis@gmail.com>
2022-04-24 21:22:46 +0300

    Optimized code

--- public/script/index.js
+++ public/script/index.js
@@ -1,22 +1,27 @@
 'use strict';
-// Title animation - Prints letters one by one at the speed of 70 ms
-// (() => {
-//     // const str = "MINDAUGAS GRIGAITIS";
-//     const str = "A Junior Front-End Web Developer based in Lithuania";
-//     const p = document.querySelector('.main-right-container > div > p:nth-child(3)');
-//     const splitStr = str.split(''); 
+let mainContainer = document.querySelector('.main-left-container');
 
-//     splitStr.forEach((e, i) => {
-//         setTimeout(() => {
-//             i > 8 ? p.innerHTML += `<span style='color: rgb(37, 37, 37);'>${e}</span>` : p.textContent += e;
-//         }, i * 50); 
-//     });
-// })();
-
-// General animations - Animate.style lib
+// Title letter print animation
 (() => {
-    const contactMe = document.querySelector('.main-left-container button');
-    contactMe.classList.add('animate__animated', 'animate__bounceIn');
+    const str = "A Junior Front-End Web Developer based in Lithuania";
+    const splitStr = str.split(''); 
+
+    splitStr.forEach((e, i) => {
+        setTimeout(() => {
+            if (i > 1 && i < 32) {
+                mainContainer.querySelector('p:last-of-type').innerHTML += `<strong>${e}</strong>`;
+            } else {
+                mainContainer.querySelector('p:last-of-type').innerHTML += e;
+            };
+        }, i * 35); 
+    });
+})();
+
+let mainButton = mainContainer.querySelector('button');
+
+// Animate.style lib
+(() => {
+    mainButton.classList.add('animate__animated', 'animate__bounceIn');
 })(); 
 
 // Footer date
@@ -26,7 +31,7 @@
 })();
 
 // Scroll to the bottom
-document.querySelector('.main-left-container button').addEventListener('click', () => {
+mainButton.addEventListener('click', () => {
     window.scrollTo({
         top: document.body.scrollHeight, // Document's entire height
         left: 0,
@@ -35,35 +40,49 @@ document.querySelector('.main-left-container button').addEventListener('click', 
 });
 
 // Mail 
-let navArr = [
-    document.querySelector('nav'),
-    document.querySelector('nav > p')
-];
-
 let form = document.getElementById('contact-me');
 let formBtn = document.querySelector('form button');
 
-// Removes the visibility of a notification after 5 seconds of being up and re-enables
-// the button 2 seconds after the pop up vanishes
+// Pop up bar
+let notification = document.querySelector('.notification');
+
+// Pop up bar children
+let children = Array.from(notification.children);
+
+// P elements of second child
+let p = children[1].children;
+
+// Removes visibility class on click or automatically
 const disableVisibility = () => {
-    setTimeout(() => {
-        navArr.forEach(e => {
-            e.classList.remove('nav-visibility');
-            setTimeout(() => {
-                formBtn.disabled = false;
-            }, 2000);
-        });
+    const visibilityTimer = setTimeout(() => {
+        notification.classList.remove('visibility');
+        formBtn.disabled = false;
     }, 5000);
+
+    notification.querySelector('.fa-xmark').addEventListener('click', () => {
+        notification.classList.remove('visibility');
+        formBtn.disabled = false;
+        clearTimeout(visibilityTimer);
+    });
 };
 
-// Notification message when email is sent successfully or fails/displays an error
-const navNotification = (color, text) => {
-    navArr.forEach(e => {
-        e.style.backgroundColor = color;
-        e.classList.add('nav-visibility');
+let iArr = [
+    notification.querySelector('i:first-of-type')
+];
+
+// Notification message
+const popup = (color, icon, toptext, bottomtext) => {
+    notification.classList.add('visibility');
+    notification.style.borderLeft = `solid 0.35rem ${color}`;
+
+    // Icon
+    iArr.forEach(e => {
+        e.classList.add('fa-solid', icon);
+        e.style.color = color;
     });
 
-    navArr[1].textContent = text;
+    p[0].innerHTML = `<strong>${toptext}</strong>`;
+    p[1].textContent = bottomtext;
 };
 
 // Creates an interval timer inside local/session storage
@@ -97,7 +116,7 @@ window.onload = () => {
         // Decrements the difference from timer
         // Difference is calculated with each new session and stored in sessionStorage
         oldValue = parseInt(localStorage.getItem('timer'));
-        difference = Math.floor((now - parseInt(localStorage.getItem('currentTime'))) / 1000)
+        difference = Math.floor((now - parseInt(localStorage.getItem('currentTime'))) / 1000);
         sessionStorage.setItem('timer', String(oldValue - difference));
     };
     
@@ -118,6 +137,7 @@ window.onload = () => {
 };
 
 form.addEventListener('submit', e => {
+    notification.querySelector('i:first-of-type').removeAttribute('class');
     if (localStorage.length === 0 || parseInt(localStorage.getItem('timer')) === 0) {
         document.querySelector('form button').textContent = 'PLEASE WAIT';
         fetch('/send-contact-form', {
@@ -130,26 +150,24 @@ form.addEventListener('submit', e => {
         .then(data => {
             if (data.success === true) {
                 localStorage.setItem('currentTime', String(Date.now()));
-                navNotification('rgb(31, 167, 31)', 'Thank you! Your submission has been sent');
+                popup('#43d55f', 'fa-circle-check', 'Success', 'Thank you! Your submission has been sent');
                 formSubmitTimer();
-                disableVisibility();
             } else {
-                navNotification('rgb(216, 54, 54)', 'Internal server error. Please try again later');
-                disableVisibility();
+                popup('#ff0000', 'fa-circle-exclamation', 'Error', 'Internal server error. Please try again later');
             };
         })
         .catch(error => {
             if (error) {
-                navNotification('rgb(214, 27, 27)', 'An error occured while sending the email message');
-                disableVisibility();
+                popup('#ff0000', 'fa-circle-exclamation', 'Error', 'An error occured while sending the email message');
             };
         }).finally(() => {
-            document.querySelector('form button').textContent = 'SUBMIT';
+                disableVisibility();
+                document.querySelector('form button').textContent = 'SUBMIT';
         });
     };
     
     if (sessionStorage.getItem('timer') && parseInt(sessionStorage.getItem('timer')) !== 0) {
-        navNotification('rgb(76, 109, 148)', `Please wait ${parseInt(sessionStorage.getItem('timer'))} ${parseInt(sessionStorage.getItem('timer')) === 1 ? 'second' : 'seconds'} before sending another email`);
+        popup('#2e86ea', 'fa-circle-info', 'Info', `Please wait ${parseInt(sessionStorage.getItem('timer'))} ${parseInt(sessionStorage.getItem('timer')) === 1 ? 'second' : 'seconds'} before sending another email`);
         disableVisibility();
     };
 
